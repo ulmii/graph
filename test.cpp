30552eea9a1e326afb018385385af61d1d7d4fe6
Konrad <veradopl@gmail.com>
2019-05-01 20:03:57 +0200

    Change test

--- test.cpp
+++ test.cpp
@@ -1,86 +1,55 @@
 #include "graph.hpp"
 #include <string>
-#include<chrono>
+#include <chrono>
 
 int main()
 {
-    auto start = std::chrono::high_resolution_clock::now();
+    Graph<std::string, double> g;
 
-    Graph<std::string, int> g;
+    for (std::size_t i = 0u; i < 6u; ++i)
+    {
+        g.insertVertex("data " + std::to_string(i));
+    }
 
-    g.edgeExist(-1, -1);
+    for (std::size_t i = 0u; i < g.nrOfVertices(); ++i)
+    {
+        for (std::size_t j = 0u; j < g.nrOfVertices(); ++j)
+        {
+            if ((i + j) & 1u || i & 1u)
+            {
+                g.insertEdge(i, j, ((i != j) ? (i + j) / 2. : 1.));
+            }
+        }
+    }
 
-    g.insertVertex("V1");
-    g.insertVertex("V2");
-    g.insertVertex("V3");
-    g.insertVertex("V4");
+    g.insertEdge(0, 2, 4.);
 
-    g.insertEdge(0, 0, 1);
-    g.insertEdge(0, 1, 2);
-    g.insertEdge(1, 2, 3);
-    g.insertEdge(2, 2, 4);
-    g.insertEdge(3, 2, 5);
-    g.insertEdge(3, 0, 6);
-    g.insertEdge(0, 3, 7);
-    g.insertEdge(1, 3, 8);
+    std::cout << (g.removeVertex(1) ? "Udalo sie" : "Nie udalo sie") << std::endl;
+    std::cout << (g.removeEdge(2, 2) ? "Udalo sie" : "Nie udalo sie") << std::endl;
+    std::cout << (g.removeEdge(2, 3) ? "Udalo sie" : "Nie udalo sie") << std::endl;
+    std::cout << (g.removeEdge(4, 3) ? "Udalo sie" : "Nie udalo sie") << std::endl;
 
-    std::cout << (g.removeEdge(0, 1) ? "Udalo sie" : "Nie udalo sie") << std::endl;
-    std::cout << (g.removeEdge(1, 0) ? "Udalo sie" : "Nie udalo sie") << std::endl;
-    std::cout << (g.removeVertex(2) ? "Udalo sie" : "Nie udalo sie") << std::endl;
-    std::cout << (g.removeVertex(5) ? "Udalo sie" : "Nie udalo sie") << std::endl;
     std::cout << "Nr of vertices: " << g.nrOfVertices() << std::endl;
     std::cout << "Nr of edges: " << g.nrOfEdges() << std::endl;
-    
+    std::cout << std::endl;
+
     g.printNeighborhoodMatrix();
 
-    for(auto it = g.beginVertices(); it != g.endVertices(); ++it) {
-        std::cout << *it << " ";
-    }
-
     std::cout << std::endl;
+    std::cout << "Vertices data:" << std::endl;
 
-    for(auto it = g.beginEdges(); it != g.endEdges(); ++it) {
-        std::cout << *it << " ";
+    for (auto v_it = g.beginVertices(); v_it != g.endVertices(); ++v_it)
+    {
+        std::cout << *v_it << ", ";
     }
+
+    std::cout << std::endl
+              << std::endl;
+
+    std::cout << "Edges data:" << std::endl;
     
-    std::cout << "\n\n";
-    
-    auto g2 = g;
-
-    g2.printNeighborhoodMatrix();
-
-    for(auto it = g2.beginVertices(); it != g2.endVertices(); ++it) {
-        std::cout << *it << " ";
+    for (auto e_it = g.beginEdges(); e_it != g.endEdges(); ++e_it)
+    {
+        std::cout << *e_it << ", ";
     }
-
-    std::cout << std::endl;
-
-    for(auto it = g2.beginEdges(); it != g2.endEdges(); ++it) {
-        std::cout << *it << " ";
-    }
-
-    std::cout << "\n\n";
-    
-    Graph<std::string, int> g3;
-
-    g3 = std::move(g2);
-
-    g3.printNeighborhoodMatrix();
-
-    for(auto it = g3.beginVertices(); it != g3.endVertices(); ++it) {
-        std::cout << *it << " ";
-    }
-
-    std::cout << std::endl;
-
-    for(auto it = g3.beginEdges(); it != g3.endEdges(); ++it) {
-        std::cout << *it << " ";
-    }
-
-    std::cout << "\n\n";
-
-    auto end = std::chrono::high_resolution_clock::now();
-
-    std::cout<< "Czas " << std::chrono::duration<double>(end-start).count() << std::endl;
-
 }
