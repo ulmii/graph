6170cec7ee8f9227ce1df5d7a81869bf29425752
Konrad <veradopl@gmail.com>
2019-05-12 16:37:46 +0200

    Add DFS function

--- test.cpp
+++ test.cpp
@@ -1,6 +1,73 @@
 #include "graph.hpp"
 #include <string>
 #include <chrono>
+#include <functional>
+#include <stack>
+
+template <typename V, typename E>
+void DFSRecur(const Graph<V, E> &graph, std::size_t start_idx, std::function<void(const V &)> func)
+{
+    std::vector<bool> visited(graph.nrOfVertices(), false);
+    visited[start_idx] = true;
+
+    std::cout << "DFS starting from point: " << start_idx << std::endl;
+    func(graph.getVertex(start_idx));
+
+    for (int i = 0; i < graph.nrOfVertices(); i++)
+        if (graph.edgeExist(start_idx, i) && !visited[i])
+            DFSUtil(visited, graph, i, func);
+
+    std::cout << std::endl;
+}
+
+template <typename V, typename E>
+void DFSUtil(std::vector<bool> &visited, const Graph<V, E> &graph, std::size_t start_idx, std::function<void(const V &)> func)
+{
+    visited[start_idx] = true;
+    func(graph.getVertex(start_idx));
+
+    for (int i = 0; i < graph.nrOfVertices(); ++i)
+        if (graph.edgeExist(start_idx, i) && !visited[i])
+            DFSUtil(visited, graph, i, func);
+}
+
+template <typename V, typename E>
+void DFS(const Graph<V, E> &graph, std::size_t start_idx, std::function<void(const V &)> func)
+{
+    std::stack<int> s; // Create a stack of ints
+    std::vector<bool> visited(graph.nrOfVertices(), false);
+
+    s.push(start_idx);         // Push the vertex onto the stack
+    visited[start_idx] = true; // Mark this node as visited
+
+    std::cout << "DFS starting from index: " << start_idx << std::endl;
+
+    if (start_idx >= graph.nrOfVertices()) // End the function if we're at our destination
+    {
+        std::cout << std::endl;
+        return;
+    }
+
+    while (!s.empty()) // While the stack is not empty
+    {
+        int k = s.top(); // Save the top element before popping it off
+        s.pop();
+
+        if (k == graph.nrOfVertices()) // Break out of the loop if we've reached our destination
+            break;
+
+        func(graph.getVertex(k));
+
+        for (int i = graph.nrOfVertices(); i >= 0; --i) // For all adjacent vertices
+            if (graph.edgeExist(k, i) && !visited[i]) // If they're connected and we haven't visited it yet
+            {
+                s.push(i); // Add it to our stack, and mark that we've visited it
+                visited[i] = true;
+            }
+    }
+
+    std::cout << std::endl;
+}
 
 int main()
 {
@@ -47,10 +114,36 @@ int main()
               << std::endl;
 
     std::cout << "Edges data:" << std::endl;
-    
+
     for (auto e_it = g.beginEdges(); e_it != g.endEdges(); ++e_it)
     {
         std::cout << *e_it << ", ";
     }
+    std::cout << std::endl
+              << std::endl;
+
+    DFS<std::string, double>(g, 0u, [](const std::string &v) -> auto {
+        std::cout << v << " ";
+    });
+    std::cout << std::endl;
+
+    DFS<std::string, double>(g, 1u, [](const std::string &v) -> auto {
+        std::cout << v << " ";
+    });
+    std::cout << std::endl;
+
+    DFS<std::string, double>(g, 2u, [](const std::string &v) -> auto {
+        std::cout << v << " ";
+    });
+    std::cout << std::endl;
+
+    DFS<std::string, double>(g, 3u, [](const std::string &v) -> auto {
+        std::cout << v << " ";
+    });
+    std::cout << std::endl;
+
+    DFS<std::string, double>(g, 4u, [](const std::string &v) -> auto {
+        std::cout << v << " ";
+    });
     std::cout << std::endl;
 }
